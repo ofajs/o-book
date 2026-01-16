3d79e4fec91a12c88deb65ce63bbd986da466011
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-16 21:32:24 +0800

    feat: 启用HCT并添加主题样式和脚本

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-importScripts("http://localhost:3002/sw/dist.js");
+if (location.host.includes("localhost")) {
+  importScripts("http://localhost:3002/sw/dist.js");
+} else {
+  importScripts("https://core.noneos.com/sw/dist.js");
+}
