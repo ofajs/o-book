b502df1109633a2a0d17f61aa8fcb732bb387369
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-25 23:43:27 +0800

    feat: 添加布局模板并更新服务工作者脚本

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
+let version = "";
+if (globalThis.serviceWorker) {
+  // 在 chrome 和 safari 内
+  // 从 serviceWorker.scriptURL 上获取 v 参数版本
+  const urlParams = new URLSearchParams(
+    new URL(serviceWorker.scriptURL).search,
+  );
+
+  version = urlParams.get("v") || "";
+} else {
+  // firefox内没有serviceWorker，则从 location 上获取 v 参数版本
+  const urlParams = new URLSearchParams(new URL(location.href).search);
+  version = urlParams.get("v") || "";
+}
+
+globalThis.SERVER_OPTIONS = {
+  useNosTool: true,
+};
+
 if (location.host.includes("localhost")) {
   importScripts("http://localhost:3002/sw/dist.js");
 } else {
-  importScripts("https://core.noneos.com/sw/dist.js");
+  importScripts("https://core.noneos.com/sw/dist.js?v=" + version);
 }
