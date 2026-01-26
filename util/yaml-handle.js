a89f0fa37d5a1a10179d9436d25843ba41a00fd8
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-26 16:08:26 +0800

    feat(仪表盘): 添加导航和文档列表功能

--- util/yaml-handle.js
+++ util/yaml-handle.js
@@ -1,6 +1,7 @@
 import yaml from "/npm/js-yaml@4.1.1/dist/js-yaml.min.mjs";
 
 export const getData = async (handle, name = "_config.yaml") => {
+  name = name.replace(/^\.\//, "");
   const configHandle = await handle.get(name);
   const configText = await configHandle.text();
   return yaml.load(configText);
