47cc7ef0d0f4937868a7c94c82d2144b03d35835
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-27 15:40:34 +0800

    backup

--- util/genarate.js
+++ util/genarate.js
@@ -1,6 +1,11 @@
+import { getData } from "./yaml-handle.js";
+
 export const initGenerator = async ({
   articleHandle, // 文章所在目录句柄
   websiteHandle, // 生成网站所在目录句柄
 }) => {
+  // 获取配置文件
+  const configData = await getData(articleHandle);
+
   debugger;
 };
