5926f7f9268db674e1547c57be831a0a8c267404
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-29 18:37:53 +0800

    feat(预览功能): 添加预览网页按钮点击事件并实现模板文件复制

--- util/genarate.js
+++ util/genarate.js
@@ -1,11 +1,50 @@
 import { getData } from "./yaml-handle.js";
 
+export const getRootUrl = () => {
+  if (location.href.includes("$mount-")) {
+    const dirId = location.href.replace(/.+\$mount-(.+)o\-book.+/, "$1");
+    return `/$mount-${dirId}o-book/`;
+  }
+
+  return "/";
+};
+
 export const initGenerator = async ({
   articleHandle, // 文章所在目录句柄
   websiteHandle, // 生成网站所在目录句柄
 }) => {
   // 获取配置文件
   const configData = await getData(articleHandle);
+
+  const tempDir = `${getRootUrl()}template/default`;
+
+  // 先复制静态模板过去
+  const files = [
+    {
+      name: "header.html",
+      path: `${tempDir}/header.html`,
+    },
+    {
+      name: "layout.html",
+      path: `${tempDir}/layout.html`,
+    },
+    {
+      name: "index.html",
+      path: `${tempDir}/index.html`,
+    },
+    {
+      name: "app-config.js",
+      path: `${tempDir}/app-config.js`,
+    },
+  ];
+
+  for (const { name, path } of files) {
+    const fileHTML = await fetch(path).then((e) => e.text());
+    const fileHandle = await websiteHandle.get(name, {
+      create: "file",
+    });
+    await fileHandle.write(fileHTML);
+  }
 
   console.log(configData);
 };
