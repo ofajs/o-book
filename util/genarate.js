2db8c4836873864d022bf28f86e75c9f76802b6f
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-30 15:32:22 +0800

    feat(生成器): 重构网站生成逻辑以支持多语言

--- util/genarate.js
+++ util/genarate.js
@@ -10,13 +10,126 @@ export const getRootUrl = () => {
 };
 
 export const initGenerator = async ({
-  articleHandle, // 文章所在目录句柄
+  websiteConfig, // 网站配置
+  topHandle, // 顶层句柄
   websiteHandle, // 生成网站所在目录句柄
 }) => {
-  // 获取配置文件
-  const configData = await getData(articleHandle);
+  // 初始化静态文件
+  await initStaticFile({
+    websiteHandle,
+  });
 
+  const { languages } = websiteConfig;
+
+  for (let lang of languages) {
+    const websiteLangHandle = await websiteHandle.get(lang, {
+      create: "dir",
+    });
+
+    const articleHandle = await topHandle.get(lang);
+
+    // 获取配置文件
+    const configData = await getData(articleHandle);
+
+    // 组合并写入配置文件的数据
+    for (const configItem of configData.header) {
+      const configData = await getData(articleHandle, configItem.url);
+
+      configItem.data = configData;
+    }
+
+    // 保存文件数据
+    const configHandle = await websiteLangHandle.get("article-config.json", {
+      create: "file",
+    });
+
+    await configHandle.write(JSON.stringify(configData));
+
+    // 遍历文件，套用内容
+    await traversingFiles({
+      fromArticleHandle: articleHandle, // 文章来源
+      toWebsiteHandle: websiteLangHandle, // 要遍历的目录句柄
+      websiteLangHandle, // 文章对应语言的顶部文件夹
+    });
+  }
+};
+
+const traversingFiles = async ({
+  fromArticleHandle,
+  toWebsiteHandle,
+  websiteLangHandle,
+}) => {
+  // 遍历文件，套用内容
+  for await (let handle of fromArticleHandle.values()) {
+    if (handle.kind === "file") {
+      if (handle.name.endsWith(".html") || handle.name.endsWith(".md")) {
+        // 需要转换的文件
+        await formatPage({
+          inputHandle: handle,
+          outputHandle: await toWebsiteHandle.get(handle.name, {
+            create: "file",
+          }),
+          websiteLangHandle,
+        });
+      }
+    } else {
+      continue;
+      // debugger;
+
+      // 目录，继续遍历内部
+      // await traversingFiles({
+      //   articleHandle: handle,
+      //   websiteLangHandle,
+      //   configData,
+      // });
+    }
+  }
+};
+
+// 将输入的文件转为对应网页
+const formatPage = async ({ inputHandle, outputHandle, websiteLangHandle }) => {
+  const content = await inputHandle.text();
+
+  // 替换主体内容
+  let outputContent = indexHTML.replace("<!-- main content -->", content);
+
+  const relatePath = outputHandle.path.replace(websiteLangHandle.path, "");
+  const floorsNum = relatePath.split("/").length - 1;
+
+  let relateStr = "";
+
+  for (let i = 0; i < floorsNum; i++) {
+    relateStr += "../";
+  }
+
+  // 更新路径资源
+  outputContent = outputContent
+    .replace(
+      ' <o-app src="./app-config.js">',
+      ` <o-app src="${relateStr}app-config.js">`,
+    )
+    .replace(
+      ' export const parent = "./layout.html";',
+      ` export const parent = "${relateStr}layout.html";`,
+    )
+    .replace(
+      `<link rel="stylesheet" href="../../css/palette.css" pui-colors />`,
+      `<link rel="stylesheet" href="${relateStr}css/palette.css" pui-colors />`,
+    )
+    .replace(
+      `<link rel="stylesheet" href="../../css/theme.css" />`,
+      `<link rel="stylesheet" href="${relateStr}css/theme.css" pui-colors />`,
+    );
+
+  await outputHandle.write(outputContent);
+};
+
+let indexHTML = "";
+
+const initStaticFile = async ({ websiteHandle }) => {
   const tempDir = `${getRootUrl()}template/default`;
+  const cssDir = `${getRootUrl()}css`;
+
   {
     // 先复制静态模板过去
     const files = [
@@ -29,28 +142,30 @@ export const initGenerator = async ({
         path: `${tempDir}/layout.html`,
       },
       {
-        name: "index.html",
-        path: `${tempDir}/index.html`,
-      },
-      {
         name: "app-config.js",
         path: `${tempDir}/app-config.js`,
       },
+      {
+        name: "palette.css",
+        path: `${cssDir}/palette.css`,
+        out: "css/palette.css",
+      },
+      {
+        name: "theme.css",
+        path: `${cssDir}/theme.css`,
+        out: "css/theme.css",
+      },
     ];
 
-    for (const { name, path } of files) {
+    for (const { name, path, out } of files) {
       const fileHTML = await fetch(path).then((e) => e.text());
-      const fileHandle = await websiteHandle.get(name, {
+      const fileHandle = await websiteHandle.get(out || name, {
         create: "file",
       });
       await fileHandle.write(fileHTML);
     }
-
-    console.log(configData);
   }
-};
 
-// 将输入的文件转为对应网页
-const formatPage = (inputHandle, outputHandle, configData) => {
-  debugger;
+  // 获取索引页HTML
+  indexHTML = await fetch(`${tempDir}/index.html`).then((e) => e.text());
 };
