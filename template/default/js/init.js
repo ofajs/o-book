1275054de275d9d1e1b76ab51ffe769588576ceb
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-02-06 18:19:16 +0800

    feat(导航): 添加文章页面的上一页下一页导航功能

--- template/default/js/init.js
+++ template/default/js/init.js
@@ -1,7 +1,55 @@
+import { getCurrentLang } from "./util.js";
+
 // 保存滚动位置
 let scrollTimer = null;
 
 export const init = async (page) => {
+  const lang = getCurrentLang();
+
+  const articleConfigData = await fetch(
+    import.meta.resolve(`../${lang}/article-config.json`),
+  ).then((res) => res.json());
+
+  {
+    // 获取上一页下一页的内容
+    const { header } = articleConfigData;
+
+    const prefixSrc = page.src.split(lang)[0];
+    const currentSrc = page.src.split(lang)[1].replace(/^\//, "");
+    const targetHeaderItem = header.find((e) =>
+      currentSrc.startsWith(e.prefix),
+    );
+
+    const flatedList = targetHeaderItem.data.flatMap((e) => e.content || [e]);
+
+    const currentIndex = flatedList.findIndex(
+      (e) => targetHeaderItem.prefix + e.url === currentSrc,
+    );
+
+    if (currentIndex !== -1) {
+      const leftItem = flatedList[currentIndex - 1];
+      const rightItem = flatedList[currentIndex + 1];
+
+      const footer = page.shadow.$(".footer");
+
+      if (leftItem) {
+        footer.push(
+          `<p-button variant="text" class="prev-item">
+          <n-icon icon="mdi:page-previous" slot="prefix"></n-icon>
+          <a href="${prefixSrc + lang}/${targetHeaderItem.prefix + leftItem.url}" olink>${leftItem.name}</a></p-button>`,
+        );
+      }
+
+      if (rightItem) {
+        footer.push(
+          `<p-button variant="text" class="next-item">
+          <n-icon icon="mdi:page-next" slot="suffix"></n-icon>
+          <a href="${prefixSrc + lang}/${targetHeaderItem.prefix + rightItem.url}" olink>${rightItem.name}</a></p-button>`,
+        );
+      }
+    }
+  }
+
   {
     // 复原
     if (sessionStorage.getItem("page_scrollTop")) {
