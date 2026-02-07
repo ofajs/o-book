c3033ba4390ad3d361aefcd3ee9cbf48484ed059
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-02-07 21:58:29 +0800

    feat: 添加搜索结果高亮和锚点定位样式

--- template/default/js/init.js
+++ template/default/js/init.js
@@ -82,9 +82,10 @@ export const init = async (page, query) => {
   {
     // 锚点修正
     if (query.index) {
-      const target = page.shadow.$("article")[query.index];
+      const target = page.shadow.$("article")[query.index - 1];
       if (target) {
         target.ele.scrollIntoView({ behavior: "smooth" });
+        target.classList.add("focus-index");
       }
     }
   }
